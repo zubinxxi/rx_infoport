94d7966378f63953cadb4996b01069400ab66d94
Zubin Juárez <zubinxxi@gmail.com>
2026-02-06 15:01:50 -0500

    Se configuró conexión con la base de datos

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,14 @@
+import os
 import reflex as rx
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+DB_CONFIG = os.getenv("DB_CONFIG")
 
 config = rx.Config(
     app_name="rx_infoport",
+    db_url=DB_CONFIG,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
